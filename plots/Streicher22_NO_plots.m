3607c80b7337329bc6d958eac88066861ccb0b4e
FlezZz23 <denvika0169@gmail.com>
2023-07-06 19:41:07 +0300

    cosmetic fixes

--- plots/Streicher22_NO_plots.m
+++ plots/Streicher22_NO_plots.m
@@ -1,129 +1,31 @@
-%addpath('..\data\Streicher experiment\');
 addpath('..\data\NO experiment\');
 load('NO_behind_ReflSW_withexch.mat');
 load('NO_between_SWs_withexch.mat');
+load('NO_Streicher22_experiment.mat');
 info=["2% No.1", "2% No.2", "2% No.3" ,"2% No.4" ,"1% No.5", "1% No.6",...
     "1% No.7","1% No.8","0.4% No.9","0.4% No.10","0.4% No.11"];
 for var=[1:11]
-%var=6; %1 - 2-02 T=3560 P=0.561;  2 - 2-14 T=5460 P=0.325; 3 - 2-32 T=7070
+%testcases     
+%var: %1 - 2-02 T=3560 P=0.561;  2 - 2-14 T=5460 P=0.325; 3 - 2-32 T=7070
 %P=0.119; 4 - 2-38 T=8730 P=0.137
 % 5 - 1-01 T=2360 P=0.757; 6 - 1-04 T=3470 P=0.584; 7 - 1-08 T=5580K P=0.252;
 % 8 - 1-12 T=7090K P=0.274; 9 - 04-02 T=2180K P=0.952; 10 - 04-11 T=3470K 
 %P=0.668;  11 - 04-22 T=5650K P=0.545;
-i_vibr=2;
-rel=2;
-switch var
-    case 1
-    TT=readmatrix('T_2%_1.csv');
-    time_T_exp=TT(:,1);
-    T_exp=TT(:,2);
-    Tv_exp=TT(:,3);
-    n_exp=readmatrix('n_2%_1.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=600;
-    err=0.045;
-    case 2
-    TT=readmatrix('T_2%_2.csv');
-    time_T_exp=TT(:,1);
-    T_exp=TT(:,2);
-    Tv_exp=TT(:,3);
-    n_exp=readmatrix('n_2%_2.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=300;
-    err=0.127;
-    case 3
-    TT=readmatrix('T_2%_3.csv');
-    time_T_exp=TT(:,1);
-    T_exp=TT(:,2);
-    Tv_exp=TT(:,3);
-    n_exp=readmatrix('n_2%_3.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=150;
-    err=0.769;
-    case 4
-    TT=readmatrix('T_2%_4.csv');
-    time_T_exp=TT(:,1);
-    T_exp=TT(:,2);
-    Tv_exp=TT(:,3);
-    n_exp=readmatrix('n_2%_4.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=70;
-    err=0.80;
-    case 5
-    TT=readmatrix('T_1%_1.csv');
-    time_T_exp=TT(:,1);
-    T_exp=TT(:,2);
-    Tv_exp=TT(:,3);
-    n_exp=readmatrix('n_1%_1.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=600;
-    err=0.013;
-    case 6
-    TT=readmatrix('T_1%_2.csv');
-    time_T_exp=TT(:,1);
-    T_exp=TT(:,2);
-    Tv_exp=TT(:,3);
-    n_exp=readmatrix('n_1%_2.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=600;
-    err=0.03;
-    case 7
-    Test=readmatrix("T_1%_3.csv");
-    time_T_exp=Test(:,1);
-    T_exp=Test(:,2);
-    Tv_exp=Test(:,3);
-    n_exp=readmatrix('n_1%_3.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=300;
-    err=0.37;
-    case 8
-    Test=readmatrix("T_1%_4.csv");
-    time_T_exp=Test(:,1);
-    T_exp=Test(:,2);
-    Tv_exp=Test(:,3);
-    n_exp=readmatrix('n_1%_3.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=150;
-    err=0.75;
-    case 9
-    Test=readmatrix("T_0_4%_1.csv");
-    time_T_exp=Test(:,1);
-    T_exp=Test(:,2);
-    Tv_exp=Test(:,3);
-    n_exp=readmatrix('n_0_4%_1.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=600;
-    err=0.02;
-    case 10
-    Test=readmatrix("T_0_4%_2.csv");
-    time_T_exp=Test(:,1);
-    T_exp=Test(:,2);
-    Tv_exp=Test(:,3);
-    n_exp=readmatrix('n_0_4%_2.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=600;
-    err=0.08;
-    case 11
-    Test=readmatrix("T_0_4%_3.csv");
-    time_T_exp=Test(:,1);
-    T_exp=Test(:,2);
-    Tv_exp=Test(:,3);    
-    n_exp=readmatrix('n_0_4%_3.csv');
-    time_n_exp=n_exp(:,1);
-    n_exp=n_exp(:,2);
-    tlim=300;
-    err=0.495;
-end
+i_vibr=2; %model of vibrational enegry exchange 1 - SSH, 2 - FHO
+rel=2; %switcher of relaxation between SWs: 1 - off, 2 - on
+
+%initialization of experimental data for temperature
+time_T_exp=data_experiment(var).T(:,1);
+T_exp=data_experiment(var).T(:,2);
+Tv_exp=data_experiment(var).T(:,3);
+
+%initialization of experimental data for number density
+time_n_exp=data_experiment(var).n(:,1);
+n_exp=data_experiment(var).n(:,2);
+tlim=max(time_n_exp);
+
+err=0.045;
+   
 
 figure("Position", [0, 0, 900, 800])
 tiledlayout(2, 2, "TileSpacing", "compact", "Padding", "tight")
