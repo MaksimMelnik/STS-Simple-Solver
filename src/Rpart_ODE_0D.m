34f0d20a2869b8c3c0ed9075b457347b9a296714
Ася Шайхутдинова <shaykhytdiniva@mail.ru>
2023-07-25 15:21:28 +0300

    replacing Rpart_ODE_0D with Rpart_0D

--- src/Rpart_ODE_0D.m
+++ src/Rpart_ODE_0D.m
@@ -1,8 +1,7 @@
-function out=Rpart_ODE_0D(t, y, kinetics)%#ok<INUSL>
+function out = Rpart_ODE_0D(~, y, kinetics)
 % Right part function for ODE systems for 0D relaxation problem.
-% t is the time; y is the vector of macroparameters;
+% y is the vector of macroparameters;
 % kinetics is the big structure with all kinetics.
-% 31.05.2023 Maksim Melnik
 
     % constants
 k = 1.380649e-23;       % Boltzmann constant, J/K
@@ -10,10 +9,15 @@ T_DN = y(end);          % dimentionless gas temperature T
 
     % relaxation terms
 [R, ~] = Rci(y, kinetics);
+R = R * kinetics.n0 * kinetics.t0;
+
     % number densities equations
-M = diag([ones(1, kinetics.num_eq) 0]);
+out = [R; 0];
+
     % energy equation
 nm = 0;                 % number density of molecules
+Rci_term = 0;
+E_term = 0;
 for ind = 1:kinetics.num_Ps
  if kinetics.Ps{ind}.fr_deg_c > 3
   e_i = [];
@@ -21,19 +25,18 @@ for ind = 1:kinetics.num_Ps
    e_i = [e_i, kinetics.Ps{ind}.e_E(ind_e) + ...
             kinetics.Ps{ind}.ev_i{ind_e} + kinetics.Ps{ind}.ev_0(ind_e)];
   end
-  M(end, kinetics.index{ind}) = ...
-                (e_i + kinetics.Ps{ind}.form_e)/k/kinetics.T0 + 2.5*T_DN;
+  E_term = E_term + ...
+        sum((R(kinetics.index{ind}))' .* (e_i + kinetics.Ps{ind}.form_e));
+  Rci_term = Rci_term + sum(R(kinetics.index{ind}));
   nm = nm + sum(y(kinetics.index{ind}));
  else
-  
-  M(end, kinetics.index{ind}) = (kinetics.Ps{ind}.form_e ...
-      + kinetics.Ps{ind}.e_E(1:kinetics.Ps{ind}.num_elex_levels))...
-                                                /k/kinetics.T0 + 1.5*T_DN;
+  E_term = E_term + ...
+      sum(R(kinetics.index{ind}) .* (kinetics.Ps{ind}.form_e + ...
+            kinetics.Ps{ind}.e_E(1:kinetics.Ps{ind}.num_elex_levels)));
  end
 end
-M(end, end) = 1.5*sum(y(1:end-1)) + nm;
-    % solving SLE
-Msp = sparse(M);
-R = [R; 0] * kinetics.n0 * kinetics.t0; % dimentionless
-out = Msp^(-1) * R;
+Rci_term = Rci_term + 1.5 * sum(R);
+n_all = sum(y(1:end-1));
+out(end) = - (T_DN * Rci_term + E_term / k / kinetics.T0) / ...
+                                                    (nm + 1.5 * n_all);
 end
