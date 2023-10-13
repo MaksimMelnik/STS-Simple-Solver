a8f9d7c87a566dbfd2ebe5ee1436252aa3562d04
Maksim Melnik <83863052+MaksimMelnik@users.noreply.github.com>
2023-10-13 22:03:52 +0300

    finished k Starik

--- src/k_exch_Starik.m
+++ src/k_exch_Starik.m
@@ -1,4 +1,4 @@
-function kd = k_exch_Starik(T, kd_eq, reaction, Ps_r, Ps_p)
+function [kd, dE_fb] = k_exch_Starik(T, kd_eq, reaction, Ps_r, Ps_p)
 % Exchange rate coefficient using the Starik model [1].
 % kd is the vector of rate coefficients.
 % M is the dissociating molecule; T is the gas temperature;
@@ -55,34 +55,11 @@ for ind_p = 1:length(Ps_p)
 end
 Ea = reaction.E;
 ET = form_e_p - form_e_r;
-dE = max(Ea, ET + Ep) - Er;
-expdE_Pneq = exp(- dE .* heaviside(dE) / k / T) .* n_eq;
+dE = (ET + Ep) - Er;
+exp_dE_Heav = exp(- dE .* heaviside(dE) / k / T);
+expdE_Pneq = exp_dE_Heav .* n_eq;
 sum_expdE_Pneq = sum(expdE_Pneq, 'all');
 A = kd_eq / sum_expdE_Pneq;
-
-% kd = A * exp(- (dE * heaviside(dE)) / T);
- 
-% k = 1.380649e-23;   % Boltzmann constant
-% zero_template = zeros(1, sum(M.num_vibr_levels(1:M.num_elex_levels)));
-% V  = zero_template; % non-equilibrium factor
-% kd = zero_template; % diss coef
-% for i = 1:M.num_elex_levels    % вот тут энергию e_i от нуля или от e_0?
-%  if M.num_vibr_levels(i) > 1
-%   %  ZvibT=sum(exp(-M.ev_i{i}/(k*T)));
-%   %  ZvibU=sum(exp(M.ev_i{i}/(k*U(i))));
-%   ZvibT = sum( exp(-(M.ev_0(i) + M.ev_i{i})/(k*T)) ); %vibrational statsum
-%   ZvibU = sum( exp( (M.ev_0(i) + M.ev_i{i})/(k*U(i))) );
-%   % V(1+sum(M.num_vibr_levels(1:i-1)):sum(M.num_vibr_levels(1:i)))=... old
-%   %                exp((M.ev_i{i}+M.e_E(i))/k*(1/T+1/U(i)))*ZvibT/ZvibU;
-%   V(1+sum(M.num_vibr_levels(1:i-1)):sum(M.num_vibr_levels(1:i))) = ...new
-%         exp((M.ev_0(i) + M.ev_i{i})/k*(1/T + 1/U(i))) * ZvibT / ZvibU;
-%   kd(1+sum(M.num_vibr_levels(1:i-1)):sum(M.num_vibr_levels(1:i))) = ...
-%       V(1+sum(M.num_vibr_levels(1:i-1)):sum(M.num_vibr_levels(1:i))) ...
-%                                                                * kd_eq(i);
-%  else
-%   kd(1+sum(M.num_vibr_levels(1:i-1)):sum(M.num_vibr_levels(1:i))) = ...
-%                                                                 kd_eq(i);
-%  end
-% end
-
+kd = A * exp_dE_Heav;       % the coefficient
+dE_fb = ET + Ep - Er;       % for the detailed balance
 end
