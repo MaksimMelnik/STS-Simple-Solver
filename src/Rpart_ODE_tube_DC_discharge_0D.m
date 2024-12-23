0b80917b5d24ad732288f02a1d260c14761ad0cc
Maksim Melnik <83863052+MaksimMelnik@users.noreply.github.com>
2024-12-23 18:52:31 +0300

    T equation fix
    Fixed concentrations of heavy particles in the T equation. That made the T profile much more realistic.

--- src/Rpart_ODE_tube_DC_discharge_0D.m
+++ src/Rpart_ODE_tube_DC_discharge_0D.m
@@ -9,18 +9,14 @@ function out=Rpart_ODE_tube_DC_discharge_0D(~, y, kinetics)
     % constants
 kb = 1.380649e-23;               % Boltzmann constant, J/K
 N_a = 6.02214076e23;    % Avogadro constant
-% ne = y(end - 1);
-ne = y(end - 2);         % dimentionless
-%T_DN = y(end);          % dimentionless gas temperature T
-% T_DN = y(end - 1);
-% T = T_DN * kinetics.T0;                           % gas temperature T
 T = y(end) * kinetics.T0;         % gas temperature T, if free_e see below
-Te = y(end)* kinetics.T0;
 
     % relaxation terms and energy flux
 y_rci = y;
 if isKey(kinetics.reactions, 'free_e') %processes involving free electrons
- T = y(end-1) * kinetics.T0;                    % gas temperature T if e
+ T  = y(end-1) * kinetics.T0;                   % gas temperature T if e
+ Te = y(end)   * kinetics.T0;
+ ne = y(end - 2);                               % dimentionless
  y_rci = y(1:end-1);
 end
 [Rh, Q] = Rci(y_rci, kinetics);                 % heavy particles
@@ -38,27 +34,34 @@ end
 Q_total = Q + Qin_e;
 
  me = 9.1094e-31;
- fr = 0;
-  for i = 1:length(kinetics.Ps)
-     mi = kinetics.Ps{i}.mass;
-     mred = mi*me/(mi+me);
-     ni = sum(y(kinetics.index{i}));
-     r = kinetics.Ps{i}.diameter / 2;
-     z = sqrt(8*pi*kb*Te/mred)*r^2;       % m3/s
-     fr = fr + z*ni/mi*kinetics.n0;      % 1/kg/s
-  end
 
     % T equation
 lambdaN2 = (1.717 + 0.084*T - 1.948e-5*T^2)/1e3;  % W / m / K
 lambdaO2 = (1.056 + 0.087*T - 8.912e-6*T^2)/1e3;  % W / m / K
 lambda = lambdaN2 * 0.8 + lambdaO2 * 0.2;         % W / m / K (kg*m/s3/K)
-n_m = sum(y(1:end-1)) * kinetics.n0 / N_a;        % molar density, mol/m3
+% n_m = sum(y(1:end-1)) * kinetics.n0 / N_a;        % molar density, mol/m3
+    % molar density, mol/m3
+n_m = sum(y(1:length(kinetics.Ps))) * kinetics.n0 / N_a;
 cp_N2 = c_p(kinetics.Ps{1}, T);
 cp_O2 = c_p(kinetics.Ps{2}, T);
 c_p_total = 0.8 * cp_N2 + 0.2 * cp_O2;      % molar heat capacity, J/mol/K
-dT = (8*lambda*(kinetics.Tw - T)/kinetics.tube_R^2 + Q_total ...
-                + 2*(Te-T)*fr*ne*me*kb*kinetics.n0) ...
-                /(n_m*c_p_total) /kinetics.T0*kinetics.t0; % dimentionless
+dT = 8*lambda*(kinetics.Tw - T)/kinetics.tube_R^2 + Q_total;
+if isKey(kinetics.reactions, 'free_e')
+    fr = 0;
+    for i = 1:length(kinetics.Ps)
+        mi = kinetics.Ps{i}.mass;
+        mred = mi*me/(mi+me);
+        ni = sum(y(kinetics.index{i}));
+        r = kinetics.Ps{i}.diameter / 2;
+        z = sqrt(8*pi*kb*Te/mred)*r^2;       % m3/s
+        fr = fr + z*ni/mi*kinetics.n0;      % 1/kg/s
+    end
+    dT = dT + 2*(Te-T)*fr*ne*me*kb*kinetics.n0;
+end
+dT = dT / (n_m*c_p_total) / kinetics.T0 * kinetics.t0;
+% dT = (8*lambda*(kinetics.Tw - T)/kinetics.tube_R^2 + Q_total ...
+%                 + 2*(Te-T)*fr*ne*me*kb*kinetics.n0) ...
+%                 /(n_m*c_p_total) /kinetics.T0*kinetics.t0; % dimentionless
 dTe = [];
     % Te equation
 if isKey(kinetics.reactions, 'free_e')
